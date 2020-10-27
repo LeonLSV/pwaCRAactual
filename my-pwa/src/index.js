81f93a7e8a1b0949ec35e5ddc79698b7c3c3a668
Ramiro Contreras León <racramiro@gmail.com>
2020-10-27 02:17:17 -0500

    Este anda

--- my-pwa/src/index.js
+++ my-pwa/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,9 +11,10 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-registerServiceWorker();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+reportWebVitals();
